ab8a8cf9f254f7c2f952c6f35e66f182b82fd9eb
Wardiman XIXV <wardimanxixv@hotmail.com>
2020-06-30 03:09:46 +0800

    add Bubble sort and Fibonacci Rekursif

---
+++ CPP_Basic/14_Fibonacci_Rekursif.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int fibonacci(int n) {
+  if (n == 0 || n == 1) {
+    return n;
+  }
+  else {
+    return (fibonacci(n - 1) + fibonacci(n - 2));
+  }
+}
+
+int main() {
+  int n, i, j = 0;
+  cout << "Masukkan batas jumlah bilangan fibonacci: "; cin >> n;
+  cout << "Hasil bilangan fibonacci: " << endl;
+  for (i = 1; i <= n; i++) {
+    cout << "fibonacci ke-" << i << ": " << fibonacci(j) << endl;
+    j++;
+  }
+  return 0;
+}
+
+// @wardiman_xixv
